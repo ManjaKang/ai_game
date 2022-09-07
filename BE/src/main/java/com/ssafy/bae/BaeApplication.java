4d27132fb5200ffb36e710cf54cdc5e09706e84e
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-09-07 16:40:11 +0900

    [BE] Create : BE 초기 폴더 생성

---
+++ BE/src/main/java/com/ssafy/bae/BaeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeApplication.class, args);
+	}
+
+}
