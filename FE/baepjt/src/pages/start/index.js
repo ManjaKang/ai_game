132bebd258aad43f11cfd73dff61393baa140952
최지은 <zzieunchoi0707@gmail.com>
2022-09-07 17:03:32 +0900

    [FE] Update: mainpage 변경

--- FE/baepjt/src/pages/start/index.js
+++ FE/baepjt/src/pages/start/index.js
@@ -1,5 +1,29 @@
-function Start() {
-  return {};
+import React from 'react';
+import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+
+function StartPage() {
+  return (
+    <ImageBackground
+      source={{
+        uri: 'https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr',
+        cache: 'only-if-cached',
+      }}
+      style={{width: '100%', height: '100%'}}>
+      <View style={styles.container}>
+        <Text style={styles.text}>화면을 터치하세요</Text>
+      </View>
+    </ImageBackground>
+  );
 }
 
-export default Start;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
+export default StartPage;
