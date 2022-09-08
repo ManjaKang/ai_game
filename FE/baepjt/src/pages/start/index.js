0e00bfd93270ae17599019f9dc570414de81edaa
최지은 <zzieunchoi0707@gmail.com>
2022-09-08 14:35:53 +0900

    [FE] ADD: start page

--- FE/baepjt/src/pages/start/index.js
+++ FE/baepjt/src/pages/start/index.js
@@ -1,28 +1,39 @@
 import React from 'react';
-import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 
 function StartPage() {
   return (
+    // 다음페이지로 넘어가기
+    // <TouchableOpacity onPress={() => Alert.alert('다음 게임 진행')}>
     <ImageBackground
       source={{
-        uri: 'https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr',
-        cache: 'only-if-cached',
+        uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKqSpCdnXNE1VVBcnKOW_yV9bzWQ5MeQCnqQ&usqp=CAU',
       }}
-      style={{width: '100%', height: '100%'}}>
+      style={{width: '100%', height: '100%'}}
+      onPress={() => navigation.navigate()}>
       <View style={styles.container}>
-        <Text style={styles.text}>화면을 터치하세요</Text>
+        <Text style={styles.text}>게임을 시작하려면 화면을 터치하세요</Text>
       </View>
     </ImageBackground>
+    // </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'flex-end',
+    marginTop: 300,
   },
   text: {
+    fontSize: 20,
     textAlign: 'center',
   },
 });
