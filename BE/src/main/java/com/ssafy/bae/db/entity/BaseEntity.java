c752a43b46e029fa4ed74dd7cedd01f376540bd9
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-09-14 17:50:03 +0900

    [BE] feat : 업적 조회 api

---
+++ BE/src/main/java/com/ssafy/bae/db/entity/BaseEntity.java
@@ -0,0 +1,19 @@
+package com.ssafy.bae.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int idx;
+}
