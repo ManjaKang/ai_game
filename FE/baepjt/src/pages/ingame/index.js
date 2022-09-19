3eceba1e47499c75badad93ba837c341b8fc8e28
최지은 <zzieunchoi0707@gmail.com>
2022-09-19 17:19:29 +0900

    [FE] Update: dialog script 넘어가기

--- FE/baepjt/src/pages/ingame/index.js
+++ FE/baepjt/src/pages/ingame/index.js
@@ -1,5 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
 import * as Progress from 'react-native-progress';
 import IngameBarLoading from '../../components/ingame/bar/loading';
 import IngameButtonItems from '../../components/ingame/button/items';
@@ -14,88 +21,94 @@ import ModalOption from '../../components/modal/option/page';
 import ModalTool from '../../components/modal/tool/page';
 
 function IngamePage(props) {
-  console.log("props data", props)
-  console.log("important",props.route.params.name);
+  console.log('props data', props);
+  console.log('important', props.route.params.name);
+  const [nameOrder, setNameOrder] = useState(0);
+
   const [isReady, setReady] = useState(false);
-  const [toolState,setToolState] = useState(false);
-  const [optionState,setOptionState] = useState(false);
-  const [dialogState,setDialogState] = useState(true);
-  const [titleState,setTitleState] = useState(false);
-  const [backlogState,setBacklogState] = useState(false);
-  const [inventoryState,setInventoryState] = useState(false);
-  const items = ["1","2"]
+  const [toolState, setToolState] = useState(false);
+  const [optionState, setOptionState] = useState(false);
+  const [dialogState, setDialogState] = useState(true);
+  const [titleState, setTitleState] = useState(false);
+  const [backlogState, setBacklogState] = useState(false);
+  const [inventoryState, setInventoryState] = useState(false);
+  const items = ['1', '2'];
   const dialog = [
     {
-      name:"name1",
-      text:"text1",
-      type:0,
-      img:0,
-      face:0,
+      name: 'name1',
+      text: 'text1',
+      type: 0,
+      img: 0,
+      face: 0,
     },
     {
-      name:"name2",
-      text:"text2",
-      type:1,
-      img:1,
-      face:0,
+      name: 'name2',
+      text: 'text2',
+      type: 1,
+      img: 1,
+      face: 0,
     },
     {
-      name:"name3",
-      text:"text3",
-      type:2,
-      img:2,
-      face:0,
+      name: 'name3',
+      text: 'text3',
+      type: 2,
+      img: 2,
+      face: 0,
     },
     {
-      name:"name3",
-      text:"text3",
-      type:2,
-      img:2,
-      face:0,
+      name: 'name3',
+      text: 'text3',
+      type: 2,
+      img: 2,
+      face: 0,
     },
     {
-      name:"name3",
-      text:"text3",
-      type:2,
-      img:2,
-      face:0,
+      name: 'name3',
+      text: 'text3',
+      type: 2,
+      img: 2,
+      face: 0,
     },
     {
-      name:"name6",
-      text:"text6",
-      type:2,
-      img:2,
-      face:0,
+      name: 'name6',
+      text: 'text6',
+      type: 2,
+      img: 2,
+      face: 0,
     },
     {
-      name:"name7",
-      text:"text7",
-      type:2,
-      img:2,
-      face:0,
+      name: 'name7',
+      text: 'text7',
+      type: 2,
+      img: 2,
+      face: 0,
     },
     {
-      name:"name8",
-      text:"text8",
-      type:2,
-      img:2,
-      face:0,
+      name: 'name8',
+      text: 'text8',
+      type: 2,
+      img: 2,
+      face: 0,
     },
     {
-      name:"name3",
-      text:"text3",
-      type:2,
-      img:2,
-      face:0,
+      name: 'name3',
+      text: 'text3',
+      type: 2,
+      img: 2,
+      face: 0,
     },
     {
-      name:"name10",
-      text:"text10",
-      type:2,
-      img:2,
-      face:0,
+      name: 'name10',
+      text: 'text10',
+      type: 2,
+      img: 2,
+      face: 0,
     },
-  ]
+  ];
+
+  const orderIncrease = () => {
+    setNameOrder(nameOrder + 1);
+  };
 
   const onFinish = () => setReady(true);
 
@@ -106,26 +119,59 @@ function IngamePage(props) {
   });
 
   return isReady ? (
-    <ImageBackground
-      source={{
-        // uri: 'https://image.shutterstock.com/image-photo/flat-lay-composition-evidences-crime-600w-1859010208.jpg',
-        uri: 'https://media.istockphoto.com/photos/yellow-police-line-do-not-cross-on-concrete-wall-background-with-copy-picture-id1366991815',
-        cache: 'only-if-cached',
-      }}
-      style={{width: '100%', height: '100%'}}>
+    <TouchableOpacity activeOpacity={1} onPress={orderIncrease}>
+      <ImageBackground
+        source={{
+          // uri: 'https://image.shutterstock.com/image-photo/flat-lay-composition-evidences-crime-600w-1859010208.jpg',
+          uri: 'https://media.istockphoto.com/photos/yellow-police-line-do-not-cross-on-concrete-wall-background-with-copy-picture-id1366991815',
+          cache: 'only-if-cached',
+        }}
+        style={{width: '100%', height: '100%'}}>
         <View style={styles.leftbox}>
-          <IngameButtonOption setstate={setOptionState}/>
+          <IngameButtonOption setstate={setOptionState} />
         </View>
-        <ModalOption visible={optionState} hideModalContentWhileAnimating={true} setter={setOptionState}/>
-        <ModalDialog visible={dialogState} hideModalContentWhileAnimating={true} setter={setDialogState}/>
+        <ModalOption
+          visible={optionState}
+          hideModalContentWhileAnimating={true}
+          setter={setOptionState}
+        />
+        <ModalDialog
+          visible={dialogState}
+          hideModalContentWhileAnimating={true}
+          setter={setDialogState}
+          data={dialog}
+          state={nameOrder}
+          setstate={setNameOrder}
+        />
+
         <View style={styles.toolbox}>
-          <IngameButtonToolbar state={toolState} setstate={setToolState}/>
-          <ModalTool state={toolState} titlestate={setTitleState} backlogstate={setBacklogState}/>
+          <IngameButtonToolbar state={toolState} setstate={setToolState} />
+          <ModalTool
+            state={toolState}
+            titlestate={setTitleState}
+            backlogstate={setBacklogState}
+          />
         </View>
-        <ModalBacklog visible={backlogState} hideModalContentWhileAnimating={true} setter={setBacklogState} data={dialog}/>
-        <ModalInventory visible={inventoryState} hideModalContentWhileAnimating={true} setter={setInventoryState} items={items}/>
-        <ModalGotomain visible={titleState} hideModalContentWhileAnimating={true} setter={setTitleState}/>
+        <ModalBacklog
+          visible={backlogState}
+          hideModalContentWhileAnimating={true}
+          setter={setBacklogState}
+          data={dialog}
+        />
+
+        <ModalInventory
+          visible={inventoryState}
+          hideModalContentWhileAnimating={true}
+          setter={setInventoryState}
+          items={items}
+        />
+        <ModalGotomain
+          visible={titleState}
+          hideModalContentWhileAnimating={true}
+          setter={setTitleState}
+        />
       </ImageBackground>
+    </TouchableOpacity>
   ) : (
     <ImageBackground
       source={{
@@ -135,7 +181,11 @@ function IngamePage(props) {
       }}
       style={{width: '100%', height: '100%'}}>
       <View style={styles.loadingbox}>
-        <IngameTextTitle name={props.route.params.name} episode={props.route.params.episode}/>
+        <IngameTextTitle
+          name={props.route.params.name}
+          episode={props.route.params.episode}
+        />
+
         <IngameBarLoading />
       </View>
     </ImageBackground>
@@ -148,20 +198,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   leftbox: {
-    height: "10%",
+    height: '10%',
     justifyContent: 'center',
     alignItems: 'center',
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
   },
   toolbox: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
     // alignItems: "flex-end",
-    justifyContent: "flex-end",
-    flexDirection: "row",
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
   },
 });
 
