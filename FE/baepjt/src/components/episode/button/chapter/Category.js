870e897520a60594a8ce498da93ceb459b5b2359
최지은 <zzieunchoi0707@gmail.com>
2022-09-20 16:36:23 +0900

    [FE] Update: each episode picture add

--- FE/baepjt/src/components/episode/button/chapter/Category.js
+++ FE/baepjt/src/components/episode/button/chapter/Category.js
@@ -8,29 +8,62 @@ import {
   TouchableOpacity,
   ImageBackground,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 function Category(props) {
   const navigation = useNavigation();
-  const imageURI = props.imageUri;
+  const isblocked = props.blocked;
   return (
     <View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('ChapterPage', {name: props.name})}>
-        <Image
-          // source={imageURI}
-          source={props.imageUri}
-          style={{
-            width: '100%',
-            height: '100%',
-            borderColor: 'white',
-            flex: 4,
-            borderRadius: 40,
-          }}></Image>
-        <View style={styles.textcontainer}>
-          <Text style={styles.text}> {props.name} </Text>
-        </View>
-      </TouchableOpacity>
+      {isblocked == 'true' ? (
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() =>
+            navigation.navigate('ChapterPage', {name: props.name})
+          }>
+          <ImageBackground
+            // source={imageURI}
+            source={props.imageUri}
+            style={{
+              width: '100%',
+              height: '100%',
+              flex: 4,
+              borderRadius: 40,
+            }}
+            imageStyle={{borderRadius: 35}}>
+            <View
+              style={{
+                marginTop: 5,
+                alignItems: 'center',
+              }}>
+              <Icon name="lock-closed-outline" size={100} color="orange" />
+            </View>
+          </ImageBackground>
+          <View style={styles.textcontainer}>
+            <Text style={styles.text}> {props.name} </Text>
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() =>
+            navigation.navigate('ChapterPage', {name: props.name})
+          }>
+          <ImageBackground
+            // source={imageURI}
+            source={props.imageUri}
+            style={{
+              width: '100%',
+              height: '100%',
+              flex: 4,
+              borderRadius: 40,
+            }}
+            imageStyle={{borderRadius: 35}}></ImageBackground>
+          <View style={styles.textcontainer}>
+            <Text style={styles.text}> {props.name} </Text>
+          </View>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
@@ -41,7 +74,6 @@ const styles = StyleSheet.create({
     width: 170,
     height: 170,
     borderRadius: 10,
-    marginLeft: 10,
     marginLeft: 10,
     backgroundColor: 'black',
   },
--- FE/baepjt/src/components/episode/button/chapter/index.js
+++ FE/baepjt/src/components/episode/button/chapter/index.js
@@ -8,29 +8,37 @@ const EpisodeButton = () => {
         <View style={{marginTop: 20}}>
           <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
             <Category
-              imageUri={require("../../../../images/뇌.jpg")}
-              name="OCD 살인"></Category>
+              imageUri={require('../../../../images/뇌.jpg')}
+              name="OCD 살인"
+              blocked="false"></Category>
             <Category
-              imageUri={require("../../../../images/뇌.jpg")}
-              name="몽환화"></Category>
+              imageUri={require('../../../../images/flower.jpg')}
+              name="몽환화"
+              blocked="true"></Category>
             <Category
-              imageUri={require("../../../../images/뇌.jpg")}
-              name="침대 위 인형"></Category>
+              imageUri={require('../../../../images/doll.jpg')}
+              name="침대 위 인형"
+              blocked="true"></Category>
             <Category
-              imageUri={require("../../../../images/뇌.jpg")}
-              name="고글 쓴 남자"></Category>
+              imageUri={require('../../../../images/goggle.jpg')}
+              name="고글 쓴 남자"
+              blocked="true"></Category>
             <Category
-              imageUri={require("../../../../images/뇌.jpg")}
-              name="백설공주"></Category>
+              imageUri={require('../../../../images/snowwhite.jpg')}
+              name="백설공주"
+              blocked="true"></Category>
             <Category
-              imageUri={require("../../../../images/뇌.jpg")}
-              name="세번째 피해자"></Category>
+              imageUri={require('../../../../images/뇌.jpg')}
+              name="세번째 피해자"
+              blocked="true"></Category>
             <Category
-              imageUri={require("../../../../images/뇌.jpg")}
-              name="은밀한 사진관"></Category>
+              imageUri={require('../../../../images/뇌.jpg')}
+              name="은밀한 사진관"
+              blocked="true"></Category>
             <Category
-              imageUri={require("../../../../images/뇌.jpg")}
-              name="붉은 낙엽"></Category>
+              imageUri={require('../../../../images/뇌.jpg')}
+              name="붉은 낙엽"
+              blocked="true"></Category>
           </ScrollView>
         </View>
       </View>
