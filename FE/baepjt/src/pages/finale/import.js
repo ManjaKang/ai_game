5017da63358480343c51c3708ba98002d21cbbe1
박지하 <bgpjh97@gmail.com>
2022-10-05 15:58:38 +0900

    [FE] Add : 마지막 챕터 분기 시스템 추가

--- FE/baepjt/src/pages/finale/import.js
+++ FE/baepjt/src/pages/finale/import.js
@@ -7,13 +7,11 @@ const FinaleimportJs = {
         require('../../images/character/limsy/face/emotionless.png'),
     ],
     DialogIndex: [
-        100, // 무능한 탐정
-        1, // 무고 : 백지현
-        2, // 무고 : 임이지
-        31, // 증거불충분
-        4, // 무고 : 임시윤
-        32, // 동기가 무엇이었을까
-        33, // 사건 해결
+        // 100, // 무능한 탐정
+        [101,1], // 무고 : 백지현
+        [101,2], // 무고 : 임이지
+        [101,31,32,33], // 증거불충분 , 동기가 무엇이었을까 , 사건 해결
+        [101,4], // 무고 : 임시윤
     ],
     selectable: [
         {
@@ -39,6 +37,7 @@ const FinaleimportJs = {
     ],
     cluelist: [5,6,8,11,14,15,17,20],
     truecluelist: [7],
+    endClueCount: [8,1],
     truesuspect: 3,
     suspectList: [{index:0, value:false},{index:1, value:false},{index:2, value:false},{index:3, value:false}],
 };
