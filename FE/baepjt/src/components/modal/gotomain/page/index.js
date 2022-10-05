10e94666b2f446a5f1aecf2328f51f5140e5ff4c
최지은 <zzieunchoi0707@gmail.com>
2022-10-05 17:28:55 +0900

    [FE] Update: 나머지 기능 확인

--- FE/baepjt/src/components/modal/gotomain/page/index.js
+++ FE/baepjt/src/components/modal/gotomain/page/index.js
@@ -51,10 +51,12 @@ const styles = StyleSheet.create({
   check: {
     fontSize: 18,
     fontFamily: 'HeirofLightRegular',
+    color: 'white',
   },
   sub: {
     fontSize: 16,
     fontFamily: 'HeirofLightRegular',
+    color: 'white',
   },
   boxset: {
     marginTop: 15,
